83c3646bcb5c17679a210eed0734530e87ae0c89
Gary <kikz270@gmail.com>
2020-12-27 12:18:37 -0600

    can now increment/decrement rest and work timers

--- src/InfoArea.js
+++ src/InfoArea.js
@@ -5,9 +5,9 @@ import InfoBar from './InfoBar'
 import './InfoArea.css'
 
 const PlayArea = ({ isTraining, isPaused }) => {
-  const [prepTimer, setPrepTimer] = useState('prep timer')
-  const [workTimer, setWorkTimer] = useState('work timer')
-  const [restTimer, setRestTimer] = useState('rest timer')
+  const [prepTimer, setPrepTimer] = useState(0)
+  const [workTimer, setWorkTimer] = useState(60)
+  const [restTimer, setRestTimer] = useState(0)
   const [sets, setSets] = useState(7)
 
   const startTimer = () => {
@@ -32,10 +32,57 @@ const PlayArea = ({ isTraining, isPaused }) => {
     }
   }
 
+  const incrementWorkTimer = () => {
+    setWorkTimer(workTimer + 1)
+  }
+
+  const decrementWorkTimer = () => {
+    if (workTimer > 0) {
+      setWorkTimer(workTimer - 1)
+    }
+  }
+
+  const incrementRestTimer = () => {
+    setRestTimer(restTimer + 1)
+  }
+
+  const decrementRestTimer = () => {
+    if (restTimer > 0) {
+      setRestTimer(restTimer - 1)
+    }
+  }
+
+  const valueToTimer = (value) => {
+    const minutes = ~~((value % 3600) / 60)
+    const seconds = ~~value % 60
+
+    let returnValue = ''
+
+    minutes < 10
+      ? returnValue += `0${minutes}`
+      : returnValue += `${minutes}`
+
+    seconds < 10
+      ? returnValue += `:0${seconds}`
+      : returnValue += `:${seconds}`
+
+    return returnValue
+  }
+
   return (
     <div className='infoArea'>
-      <InfoBar label='Rest' info='00:20' />
-      <InfoBar label='Work' info='01:00' />
+      <InfoBar
+        label='Rest'
+        info={valueToTimer(restTimer)}
+        incrementFunction={incrementRestTimer}
+        decrementFunction={decrementRestTimer}
+      />
+      <InfoBar
+        label='Work'
+        info={valueToTimer(workTimer)}
+        incrementFunction={incrementWorkTimer}
+        decrementFunction={decrementWorkTimer}
+      />
       <InfoBar
         label='Sets'
         info={sets}
